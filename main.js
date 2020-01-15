ac5eb56deb4e2e7b53d08d1747714a2cedf0aec2
Marcos <marcv56@hotmail.com>
2020-01-15 15:26:47 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 (function(){
-  }
+  'use strict';
 
-  let $elemnt= document.queryselector ('span')
+  // let $element= document.querySelector('span');
+  //
+  // $element.textContent = "hello sally "
 
-  $element. textcontent="hello sally "
+let $elementId = document.querySelector('#myid')
+
+// let date = new Date()
+// console.log(date);
+//
+// let currentTime;
+// // set inveral(function,ms);
+// // setInterval(() => {
+// //   currentTime=new Date();
+// //   console.log(currentTime);
+// // },1000);
+
+let $button = document.querySelector('button');
+$button.addEventListener('click', () => {
+  console.log(`you clicked a button`);
+})
+
+
+})();
