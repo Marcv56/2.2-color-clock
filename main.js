8107f6b3c62e70c0fa6da8befcfe38b458e6577d
Marcos <marcv56@hotmail.com>
2020-01-16 14:49:29 -0500

    style update

--- main.js
+++ main.js
@@ -1,26 +1,77 @@
-(function(){
-  'use strict';
+var $clock = document.getElementById('clock');
+var $time = document.getElementById('time');
 
-  // let $element= document.querySelector('span');
-  //
-  // $element.textContent = "hello sally "
+function hexClock() {
+  var time = new Date();
+  var hours = time.getHours().toString();
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
 
-let $elementId = document.querySelector('#myid')
+  if (hours.length < 2) {
+    hours = '0' + hours;
+  }
+  if (minutes.length < 2) {
+    minutes = '0' + hours;
+  }
+  if (seconds.length < 2) {
+    seconds = '0' + hours;
+  }
+
+  var clockStr = hours + ':' + minutes + ':' + seconds;
+  var hexColorStr = '#' + hours + minutes + seconds; // you still need to generate a hexCode
+
+
+  $time.textContent = clockStr;
+  // hexColor.textContent = hexColorStr
+  document.body.style.backgroundColor = hexColorStr;
+
+}
+
+setInterval(hexClock, 1000);
 
 // let date = new Date()
 // console.log(date);
 //
 // let currentTime;
-// // set inveral(function,ms);
-// // setInterval(() => {
-// //   currentTime=new Date();
-// //   console.log(currentTime);
-// // },1000);
+//  set inveral(function,ms);
+// setInterval(() => {
+//   currentTime=new Date();
+//   console.log(currentTime);
+//  },1000);
 
-let $button = document.querySelector('button');
-$button. addEventListner('click', () => {
-  console.log(`you clicked a button`);
-})
+// hexClock()
 
 
-})();
+
+
+
+
+
+
+
+// (function(){
+//   'use strict';
+//
+//   // let $element= document.querySelector('span');
+//   //
+//   // $element.textContent = "hello sally "
+//
+// let $elementId = document.querySelector('#myid')
+//
+// let date = new Date()
+// console.log(date);
+//
+// let currentTime;
+//  set inveral(function,ms);
+// setInterval(() => {
+//   currentTime=new Date();
+//   console.log(currentTime);
+//  },1000);
+//
+// let $button = document.querySelector('button');
+// $button. addEventListner('click', () => {
+//   console.log(`you clicked a button`);
+// })
+//
+//
+// })();
